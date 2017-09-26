b38cabdf355a25650a7c88cb158ca7215e531629
joaovenancio <joaovitorvenancio99@outlook.com>
2017-09-26 10:55:49 -0300

    Criado um pacote para as Interfaces e movido a outra interface

--- SistemaControleAcesso/src/br/ufsc/ine5605/sistemacontroleacesso/interfaces/ICargo.java
+++ SistemaControleAcesso/src/br/ufsc/ine5605/sistemacontroleacesso/interfaces/ICargo.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.ufsc.ine5605.sistemacontroleacesso;
+package br.ufsc.ine5605.sistemacontroleacesso.interfaces;
 
 /**
  *
