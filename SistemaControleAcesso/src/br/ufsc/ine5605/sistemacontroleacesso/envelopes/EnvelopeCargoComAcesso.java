d388068e5ff4a3bd89678173b237872513a9864f
peaugust <peaugust@hotmail.com>
2017-10-07 20:51:21 -0300

    Fazendo algumas alterações na classe EnvelopeCargoComAcesso -- implementando construtor

--- SistemaControleAcesso/src/br/ufsc/ine5605/sistemacontroleacesso/envelopes/EnvelopeCargoComAcesso.java
+++ SistemaControleAcesso/src/br/ufsc/ine5605/sistemacontroleacesso/envelopes/EnvelopeCargoComAcesso.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -25,6 +25,8 @@ public class EnvelopeCargoComAcesso {
 	public EnvelopeCargoComAcesso(String codigo, String nome, Calendar inicio, Calendar fim) {
 		this.codigo = codigo;
 		this.nome = nome;
+                this.arrayComHorarios = new ArrayList<>();
+                
 	}
     
 }
