885a8b443882c0ada5b9d081e4b4cb6223c65ec2
peaugust <peaugust@hotmail.com>
2017-10-16 10:16:34 -0200

    Criando atributos e construtor do envelope

--- SistemaControleAcesso/src/br/ufsc/ine5605/sistemacontroleacesso/envelopes/EnvelopeRegistro.java
+++ SistemaControleAcesso/src/br/ufsc/ine5605/sistemacontroleacesso/envelopes/EnvelopeRegistro.java
@@ -5,10 +5,22 @@
  */
 package br.ufsc.ine5605.sistemacontroleacesso.envelopes;
 
+import br.ufsc.ine5605.sistemacontroleacesso.AcontecimentoRegistro;
+import java.util.Calendar;
+
 /**
  *
  * @author PEaug
  */
 public class EnvelopeRegistro {
+    private AcontecimentoRegistro acontecimento;
+    private Calendar dataAcontecimento;
+    private int numDeMatricula;
     
+    public EnvelopeRegistro(AcontecimentoRegistro acontecimento, 
+            Calendar dataAcontecimento, int numDeMatricula) {
+        this.acontecimento = acontecimento;
+        this.dataAcontecimento = dataAcontecimento;
+        this.numDeMatricula = numDeMatricula;
+    }
 }
