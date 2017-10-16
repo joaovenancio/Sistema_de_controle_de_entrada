64f1e19d2ee01713a5432824c43c8b3ce4b29285
peaugust <peaugust@hotmail.com>
2017-10-16 00:05:16 -0200

    Tirei os sysout do controlador e coloquei um retorno de String

--- SistemaControleAcesso/src/br/ufsc/ine5605/sistemacontroleacesso/controladores/ControladorPortaFinanceiro.java
+++ SistemaControleAcesso/src/br/ufsc/ine5605/sistemacontroleacesso/controladores/ControladorPortaFinanceiro.java
@@ -4,9 +4,11 @@
  * and open the template in the editor.
  */
 package br.ufsc.ine5605.sistemacontroleacesso.controladores;
+
 import br.ufsc.ine5605.sistemacontroleacesso.AcontecimentoRegistro;
 import br.ufsc.ine5605.sistemacontroleacesso.CargoComAcesso;
 import br.ufsc.ine5605.sistemacontroleacesso.Funcionario;
+import br.ufsc.ine5605.sistemacontroleacesso.Registro;
 import br.ufsc.ine5605.sistemacontroleacesso.telas.TelaPortaFinanceiro;
 import java.util.Calendar;
 
@@ -15,32 +17,34 @@ import java.util.Calendar;
  * @author Joao Vitor Venancio
  */
 public class ControladorPortaFinanceiro {
+
     //Atributos
     private ControladorGeral controladorGeral;
     private TelaPortaFinanceiro telaPortaFinanceiro;
-    
+
     //Construtor
     public ControladorPortaFinanceiro(ControladorGeral controlador) {
         this.controladorGeral = controlador;
         this.telaPortaFinanceiro = new TelaPortaFinanceiro(this);
     }
-    
+
     //Metodos
     /**
-     * Mostra uma interface grafica para o usuario interagir. Nele ele pode selecionar
-     * o que quer fazer na porta.
+     * Mostra uma interface grafica para o usuario interagir. Nele ele pode
+     * selecionar o que quer fazer na porta.
      */
     public void iniciarTela() {
         this.telaPortaFinanceiro.iniciar();
     }
 
     /**
-     * Tenta abrir a porta. Caso o a matricula do funcionario não for valida, ele joga uma excecao.
-     * 
+     * Tenta abrir a porta. Caso o a matricula do funcionario não for valida,
+     * ele joga uma excecao.
+     *
      * @param numeroDeMatricula para o usuario tentado destrancar a porta.
      * @param horario da data atual.
      */
-    public void abrirPorta(int numeroDeMatricula, Calendar horario) {
+    public String abrirPorta(int numeroDeMatricula, Calendar horario) {
         //Verificar se o numero de matricula existe:
         boolean existe = false;
         Funcionario funcionarioPorta = null;
@@ -52,28 +56,23 @@ public class ControladorPortaFinanceiro {
             }
         }
         //Se nao existir, joga uma excecao;
-        if(!(existe)) {
+        if (!(existe)) {
             throw new IllegalArgumentException("Usuario com essa matricula nao existe");
         }
-        
+
         //Tentar acesso a porta:
-        
         //Metodo em andamento
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        
-        
-        //Verificar se o usuario em questao possui acesso a aporta:
-        if (funcionarioPorta.getCargo().temAcesso(horario)) {
-            System.out.println("Acesso autorizado.");
-        }else{
-        	if(funcionarioPorta.getCargo() instanceof CargoComAcesso){
-            	System.out.println(AcontecimentoRegistro.FORADEHORARIO.getDescricao());
-        	}else{
-            	System.out.println(AcontecimentoRegistro.CARGOSEMACESSO.getDescricao());
+        //Verificar se o usuario em questao possui acesso a a porta:
+        if (funcionarioPorta.getCargo() instanceof CargoComAcesso) {
+            controladorGeral.getControladorRegistros().addRegistro(registro);
+            return AcontecimentoRegistro.FORADEHORARIO.getDescricao();
+        } 
+        if (funcionarioPorta.getCargo() instanceof CargoComAcesso){
+            return AcontecimentoRegistro.CARGOSEMACESSO.getDescricao();
 
-        	}
-        	
-            //System.out.println("Acesso não autorizado.");
         }
+        return "Acesso Autorizado";
     }
+    
 }
